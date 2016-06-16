259eea7351ad85f19df176e8911485db56f551fc
CuriosityEngineer <aeshta@gmail.com>
2016-06-16 21:35:12 +0200

    2e branch for CSS

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,12 @@ ActiveRecord::Schema.define(version: 20160616115710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "bookings", force: :cascade do |t|
     t.string   "name"
     t.datetime "start_date"
@@ -56,6 +62,15 @@ ActiveRecord::Schema.define(version: 20160616115710) do
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "artist_id"
+  end
+
+  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
