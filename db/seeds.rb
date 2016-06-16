3da5780749042ccf156aa7657bb0ca94b28c6793
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-16 14:55:13 +0200

    Fixed environment issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user1 = User.create(email: "stefan@stefan.com", password: "password123")
-
-profile_one = Profile.create(user: user1, avatar: "http://ichef.bbci.co.uk/news/976/mcs/media/images/82377000/jpg/_82377877_dogthinkstock.jpg", name: "Stefan Ouweneel", description: "Loves puppies")
-
-listing_one = Listing.create(price: 500, name: "Monopoly", avatar: "https://upload.wikimedia.org/wikipedia/en/f/f9/Monopoly_pack_logo.png", start_date: Time.now, end_date: Time.now + 2.weeks, description: "It's awesome", difficulty: 5, players: 6, user: user1)
-
-Booking.create(name: "Random", start_date: Time.now + 1.week, end_date: Time.now + 10.weeks, price: 500, user: user1, listing: listing_one)
-
-
 user2 = User.create(email: "test@test.com", password: "password123")
 
+profile_one = Profile.create(user: user1, avatar: "http://ichef.bbci.co.uk/news/976/mcs/media/images/82377000/jpg/_82377877_dogthinkstock.jpg", name: "Stefan Ouweneel", description: "Loves puppies")
 profile_two = Profile.create(user: user2, avatar: "http://lovelace-media.imgix.net/uploads/191/0f832b40-d622-0132-cea9-0e01949ad350.jpg", name: "Stefan Ouweneels", description: "Loves puppies more")
 
+listing_one = Listing.create(price: 500, name: "Monopoly", avatar: "https://upload.wikimedia.org/wikipedia/en/f/f9/Monopoly_pack_logo.png", start_date: Time.now, end_date: Time.now + 2.weeks, description: "It's awesome", difficulty: 5, players: 6, user: user1)
 listing_two = Listing.create(price: 50, name: "Small World", avatar: "https://cf.geekdo-images.com/images/pic428828_md.jpg", start_date: Time.now, end_date: Time.now + 2.weeks, description: "It's super awesome", difficulty: 1, players: 87, user: user2)
 
+Booking.create(name: "Random", start_date: Time.now + 1.week, end_date: Time.now + 10.weeks, price: 500, user: user1, listing: listing_one)
 Booking.create(name: "Random", start_date: Time.now + 2.days, end_date: Time.now + 7.days, price: 50, user: user2, listing: listing_two)
