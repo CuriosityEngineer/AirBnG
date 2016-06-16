f387312431fe6adbf5b5ffcfab2e592b4de9b4d3
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-16 18:21:23 +0200

    Merge frontend with backend

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160616115710) do
-=======
-ActiveRecord::Schema.define(version: 20160615151319) do
->>>>>>> 68c7fbafcdd57fe8412464da661dacd98418542a
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "bookings", force: :cascade do |t|
     t.string   "name"
@@ -66,15 +56,6 @@ ActiveRecord::Schema.define(version: 20160615151319) do
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
-
-  create_table "songs", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "artist_id"
-  end
-
-  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
